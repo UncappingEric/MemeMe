0443fb507820ff10596d5d9727d9a69e33c28ff5
Eric Cajuste <eric_cajuste2000@yahoo.com>
2017-10-13 00:53:45 -0400

    Now has saving/sharing capability.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -11,9 +11,30 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var topField: UITextField!
     @IBOutlet weak var bottomField: UITextField!
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    @IBOutlet weak var topToolbar: UIToolbar!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    var savedMeme: Meme?
+    
+    //MARK: Meme Structure
+    
+    struct Meme {
+        var top: String
+        var bottom: String
+        var original: UIImage
+        var meme: UIImage
+        
+        init(top: String, bottom: String, original: UIImage, meme: UIImage) {
+            self.top = top
+            self.bottom = bottom
+            self.original = original
+            self.meme = meme
+        }
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -33,6 +54,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         bottomField.defaultTextAttributes = memeTextAttributes
         bottomField.textAlignment = .center
         bottomField.delegate = self
+        shareButton.isEnabled = false
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -46,6 +68,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // MARK: PhotoPicker Methods
+    
     @IBAction func photoPickerFromAlbum(_ sender: Any) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
@@ -60,14 +84,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: Delegate Functions
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = image
-            imageView.contentMode = .scaleToFill
+            //imageView.contentMode = .scaleToFill
         }
         
-        dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: {self.shareButton.isEnabled = true})
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case topField:
+            unsubscribeFromKeyboardNotifications()
+            if textField.text == "TOP" {
+                textField.text = ""
+            }
+        default:
+            if textField.text == "BOTTOM" {
+                textField.text = ""
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == topField {
+            subscribeToKeyboardNotifications()
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Keyboard Functions
     
     @objc func keyboardWillShow(_ notification:Notification) {
         view.frame.origin.y = 0 - getKeyboardHeight(notification)
@@ -100,26 +150,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        switch textField {
-        case topField:
-            unsubscribeFromKeyboardNotifications()
-            if textField.text == "TOP" {
-                textField.text = ""
-            }
-        default:
-            if textField.text == "BOTTOM" {
-                textField.text = ""
-            }
-        }
+    // MARK: Saving Functions
+    
+    func saveMeme() {
+        savedMeme = Meme(top: topField.text!, bottom: bottomField.text!, original: imageView.image!, meme: generateMeme())
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == topField {
-            subscribeToKeyboardNotifications()
-        }
-        textField.resignFirstResponder()
-        return true
+    func generateMeme() -> UIImage {
+        
+        topToolbar.isHidden = true
+        bottomToolbar.isHidden = true
+        navigationController?.navigationBar.isHidden = true
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let meme:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        
+        topToolbar.isHidden = false
+        bottomToolbar.isHidden = false
+        navigationController?.navigationBar.isHidden = false
+        
+        return meme
+    }
+    
+    @IBAction func shareMeme() {
+        let meme = generateMeme()
+        
+        let controller = UIActivityViewController(activityItems: [meme], applicationActivities: nil)
+        present(controller, animated: true, completion:{self.saveMeme()})
     }
 }
 
